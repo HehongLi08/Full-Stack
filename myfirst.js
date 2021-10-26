88b94c724d092e182f58b3a172f62df40e184cc1
Myosotis <g20170282@icloud.com>
2021-10-25 22:36:01 -0400

    m

--- myfirst.js
+++ myfirst.js
@@ -1,20 +1,17 @@
 var http = require("http");
 var url = require("url");
+var fs = require("fs")
 var dt = require("./myfirstmodule")
 
 
 http.createServer( function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("this is a test!!!</br>");
-    res.write("The curr date and time are : " + dt.myDateTime()  + "</br>");
 
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
+    fs.readFile("./demofile1.html", function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
 
-
-
-    console.log("test!!!");
-
-    res.end(txt);
+    console.log("already provided the HTML file!");
 
 }).listen(8080);
