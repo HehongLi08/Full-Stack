8d8de72cdcd70c3f356bdb19c188fcac5eac047f
Myosotis <g20170282@icloud.com>
2021-10-25 22:47:43 -0400

    new test

--- myfirst.js
+++ myfirst.js
@@ -1,15 +1,13 @@
 var http = require("http");
 var url = require("url");
-var fs = require("fs")
-var dt = require("./myfirstmodule")
-
+var fs = require("fs");
+var dt = require("./myfirstmodule");
 
 http.createServer( function(req, res) {
 
-    fs.readFile("./demofile1.html", function (err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
+    fs.appendFile("mynewfile1.txt", "Hello content!", function(err) {
+        if (err) throw err;
+        console.log("Saved!");
     });
 
     console.log("already provided the HTML file!");
