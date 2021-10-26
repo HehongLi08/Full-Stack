3524ce163464137dda2a46d90f5572321595710a
Myosotis <g20170282@icloud.com>
2021-10-25 22:52:18 -0400

    try url file

--- myfirst.js
+++ myfirst.js
@@ -1,15 +1,21 @@
 var http = require("http");
 var url = require("url");
 var fs = require("fs");
-var dt = require("./myfirstmodule");
 
 http.createServer( function(req, res) {
+    console.log("received a request!");
 
-    fs.appendFile("mynewfile1.txt", "Hello content!", function(err) {
-        if (err) throw err;
-        console.log("Saved!");
+    var q = url.parse(req.url);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+       if (err) {
+           res.writeHead(404, {'Content-Type': 'text/html'});
+           return res.end("404 Not Found");
+       }
+       res.writeHead(200, {'Content.Type': 'text/html'});
+       res.write(data);
+       return res.end();
     });
 
-    console.log("already provided the HTML file!");
-
+    console.log("request done!");
 }).listen(8080);
